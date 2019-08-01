0889e1597e8a84f55c4891d3a65c2d8b8e7c7741
Guillermo Villalta <mexcelus@gmail.com>
2019-08-01 16:12:21 -0600

    added timeConverter method into trivia object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,7 +36,27 @@ var trivia = {
             ]
         }
        
-   ] 
+   ],
+    timer: function timeConverter(t) {
+
+        //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes === 0) {
+            minutes = "00";
+        }
+
+        else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
+    } 
 };
 
 console.log(trivia.QandA[1].Question);
